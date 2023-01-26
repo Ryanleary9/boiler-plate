237c5f50671dd74573d5c6c04f8ea94eb8ededea
Ryan <ryanlearyzafra@gmail.com>
2023-01-26 13:36:58 +0100

    Changen name from master to main

--- sample.test.js
+++ sample.test.js
@@ -2,10 +2,10 @@ import { add } from './sample';
 
 describe('Given add function', () => {
   test('When given 1 + 1 should give 2', () => {
-    const a = 1;
+    const c = 1;
     const b = 3;
     const expected = 4;
-    const r = add(a, b);
+    const r = add(c, b);
     expect(r).toBe(expected);
   });
 });
